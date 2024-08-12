cba22b24a13f22363b1f252ca0ef1f46a42fcbef
iam8uman <me8848you@gmail.com>
2024-08-12 23:54:13 +0545

    basic setup completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+import Sidebar from "@/lib/layout/Sidebar";
+import Topbar from "@/lib/layout/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+          <Sidebar />
+          <div className="flex flex-col">
+            <Topbar />
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,11 +1,13 @@
-import Dashboard from "@/components/dashboard";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+const page = () => {
   return (
-    <>
+    <div>
       <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+        </div>
         <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
           <div className="flex flex-col items-center gap-1 text-center">
             <h3 className="text-2xl font-bold tracking-tight">
@@ -18,6 +20,8 @@ export default function Home() {
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
-}
+};
+
+export default page;
