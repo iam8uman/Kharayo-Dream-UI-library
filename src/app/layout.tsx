396a0e91bf1f8a7d2c56be2fd9f1c80b3dd7c8b9
iam8uman <me8848you@gmail.com>
2024-08-23 12:22:25 +0545

    SEO Optimize images and link we updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,36 +2,49 @@ import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
 
-const inter = Ubuntu({ weight: "400", subsets: ["latin"] });
+const ubuntu = Ubuntu({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SUI | UI library powered by WSC",
-  description: "Useful UI components based on tailwindcss and shacnUI",
+  title: "SUI | Your Dream UI Library for Next.js, Tailwind CSS, ShadcnUI and Framer Motion",
+  description: "Build responsive and accessible UI components with SUI - a powerful library combining Next.js, Tailwind CSS, shadcn/ui, and Framer Motion for seamless web development.",
+  keywords: "UI library, Next.js, Tailwind CSS, shadcn/ui, Framer Motion, responsive design, accessibility, web development",
+  authors: [{ name: "Suman Sharma" }],
+  openGraph: {
+    title: "SUI | Modern UI Library for Next.js and Tailwind CSS",
+    description: "Create stunning web interfaces with SUI - combining Next.js, Tailwind CSS, and Framer Motion for powerful, responsive designs.",
+    type: "website",
+    url: "https://sui.whysumancode.com",
+    images: "https://sui.whysumancode.com/og-image.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SUI | Next.js UI Library",
+    description: "Elevate your web development with SUI - a modern UI library for Next.js and Tailwind CSS.",
+    images: "https://sui.whysumancode.com/twitter-image.jpeg",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    // <KindeProvider>
     <html lang="en">
-      <body
-        className={`${inter.className} bg-slate-200 dark:bg-slate-950 text-foreground`}
-      >
+      <head>
+        <link rel="canonical" href="https://sui.whysumancode.com" />
+      </head>
+      <body className={`${ubuntu.className} bg-slate-200 dark:bg-slate-950 text-foreground`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <main>{children}</main>
         </ThemeProvider>
       </body>
     </html>
-    // </KindeProvider>
   );
 }
